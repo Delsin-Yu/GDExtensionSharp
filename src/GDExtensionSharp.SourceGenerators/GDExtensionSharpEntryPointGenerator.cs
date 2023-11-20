e5e1f510b864a6c9d0ad928310a252cd03349288
Summpot <summpot@outlook.com>
2023-11-20 23:29:32 +0800

    Add entrypoint generator

---
+++ src/GDExtensionSharp.SourceGenerators/GDExtensionSharpEntryPointGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace GDExtensionSharp.SourceGenerators
+{
+    [Generator]
+
+    internal class GDExtensionSharpEntryPointGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(initializationContext =>
+            {
+                initializationContext.AddSource("GDExtensionSharpEntryPointAttribute.g.cs", """
+                    namespace GDExtensionSharp;
+
+                    [System.AttributeUsage(AttributeTargets.Class)]
+                    internal class GDExtensionSharpEntryPointAttribute : System.Attribute
+                    {
+                        public GDExtensionSharpEntryPointAttribute(string name) { }
+                    }
+                    """);
+            });
+        }
+    }
+}
