2aa398081785983d310c9b978fcf9d147425d40d
DE-YU <delsin_yu@qq.com>
2023-11-24 17:18:21 +0800

    Code cleanup

--- src/GDExtensionSharp.SourceGenerator.Api/ApiGeneratorImpl/ApiGenerator.cs
+++ src/GDExtensionSharp.SourceGenerator.Api/ApiGeneratorImpl/ApiGenerator.cs
@@ -19,11 +19,6 @@ internal static partial class ApiGenerator
 
         yield return GenerateGlobalUsing();
 
-        foreach (var generatedGlobalEnum in GenerateGlobalEnums(stringBuilder, gdExtensionApi.GlobalEnums))
-        {
-            yield return generatedGlobalEnum;
-        }
-
         var size =
             gdExtensionApi
                 .BuiltinClassSizes
@@ -33,33 +28,17 @@ internal static partial class ApiGenerator
 
         yield return GenerateFixedSizeBuffer(stringBuilder, size);
 
+
+        foreach (var generatedGlobalEnum in GenerateGlobalEnums(stringBuilder, gdExtensionApi.GlobalEnums))
+        {
+            yield return generatedGlobalEnum;
+        }
+
         foreach (var generatedBuiltinClass in GenerateBuiltinClasses(stringBuilder, gdExtensionApi.BuiltinClasses, size))
         {
             yield return generatedBuiltinClass;
         }
 
         stringBuilder.Clear();
-    }
-
-
-    private static (string sourceName, string sourceContent) GenerateGlobalUsing()
-    {
-        const string content =
-            """
-            global using int8_t = System.SByte;
-            global using int16_t = System.Int16;
-            global using int32_t = System.Int32;
-            global using int64_t = System.Int64;
-            global using uint8_t = System.Byte;
-            global using uint16_t = System.UInt16;
-            global using uint32_t = System.UInt32;
-            global using uint64_t = System.UInt64;
-
-            global using size_t = System.UInt64;
-
-            global using wchar_t = System.UInt16;
-            """;
-
-        return ("GlobalUsing", content);
     }
 }
